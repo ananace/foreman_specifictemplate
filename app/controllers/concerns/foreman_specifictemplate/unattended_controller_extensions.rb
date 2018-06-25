258200d590e53491ec093ea3fb6437216bdd9c43
Alexander Olofsson <alexander.olofsson@liu.se>
2018-06-25 16:34:45 +0200

    Use early exit for host_template

--- app/controllers/concerns/foreman_specifictemplate/unattended_controller_extensions.rb
+++ app/controllers/concerns/foreman_specifictemplate/unattended_controller_extensions.rb
@@ -1,19 +1,17 @@
 module ForemanSpecifictemplate
   module UnattendedControllerExtensions
     def host_template
-      if params[:kind] == 'specifictemplate'
-        if params[:redirect] == 'true'
-          redirect_to url_for(controller: :specifictemplate, action: :update, template_name: params[:template_name])
-        else
-          controller = SpecifictemplateController.new
-          controller.request = @_request
-          controller.response = @_response
-          controller.params = params
-          controller.process(:update)
-          render text: controller.response.body, status: controller.response.response_code, content_type: controller.response.content_type
-        end
+      return super unless params[:kind] == 'specifictemplate'
+
+      if params[:redirect] == 'true'
+        redirect_to url_for(controller: :specifictemplate, action: :update, template_name: params[:template_name])
       else
-        super
+        controller = SpecifictemplateController.new
+        controller.request = @_request
+        controller.response = @_response
+        controller.params = params
+        controller.process(:update)
+        render text: controller.response.body, status: controller.response.response_code, content_type: controller.response.content_type
       end
     end
 
